013cbd752dc7f3fdbc90c885928c2ddd0b79e844
fourbarman <maks.java@yandex.ru>
2026-03-24 13:15:07 +0400

    companion object

--- src/main/kotlin/ru/job4j/oop/tracker/StartUI.kt
+++ src/main/kotlin/ru/job4j/oop/tracker/StartUI.kt
@@ -4,54 +4,56 @@ import ru.job4j.oop.tracker.item.Item
 import ru.job4j.oop.tracker.tracker.Tracker
 import java.util.UUID
 
-object StartUI {
-    private val tracker = Tracker()
+class StartUI private constructor() {
+    companion object {
+        private val tracker = Tracker()
 
-    private var run = true
-    fun init() {
-        while (run) {
-            showMenu()
-            when(readln().toIntOrNull()) {
-                1 -> addItem()
-                2 -> showAllItems()
-                3 -> exit()
-                else -> println("Wrong input")
+        private var run = true
+        fun init() {
+            while (run) {
+                showMenu()
+                when (readln().toIntOrNull()) {
+                    1 -> addItem()
+                    2 -> showAllItems()
+                    3 -> exit()
+                    else -> println("Wrong input")
+                }
             }
         }
-    }
 
-    private fun showMenu() {
-        println()
-        println("Menu")
-        println("1. Add item")
-        println("2. Show all items")
-        println("3. Exit")
-        print("Provide number: ")
-    }
+        private fun showMenu() {
+            println()
+            println("Menu")
+            println("1. Add item")
+            println("2. Show all items")
+            println("3. Exit")
+            print("Provide number: ")
+        }
 
-    private fun addItem() {
-        print("Enter item name: ")
-        val name = readln()
-        val item = Item(name, UUID.randomUUID())
-        tracker.add(item)
-        println("New item added: $item")
-    }
+        private fun addItem() {
+            print("Enter item name: ")
+            val name = readln()
+            val item = Item(name, UUID.randomUUID())
+            tracker.add(item)
+            println("New item added: $item")
+        }
 
-    private fun showAllItems() {
-        val items = tracker.findAll()
-        if (items.isEmpty()) {
-            println("No item in storage")
-        } else {
-            println("Found items:")
-            for (item in items) {
-                println("Name: ${item.name}, UUID: ${item.uuid}")
+        private fun showAllItems() {
+            val items = tracker.findAll()
+            if (items.isEmpty()) {
+                println("No item in storage")
+            } else {
+                println("Found items:")
+                for (item in items) {
+                    println("Name: ${item.name}, UUID: ${item.uuid}")
+                }
             }
         }
-    }
 
-    private fun exit() {
-        println("Exiting tracker")
-        run = false
+        private fun exit() {
+            println("Exiting tracker")
+            run = false
+        }
     }
 }
 
